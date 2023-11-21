86145fdcaf398f63c9f037f1980f1d6921f0d298
agusmaulanayusufnoor <agusmaulanayusufnoor@gmail.com>
2023-11-21 22:49:58 +0700

    ico

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -43,7 +43,7 @@ class AdminPanelProvider extends PanelProvider
                 'primary' => Color::Amber,
             ])
             ->brandLogo(asset('images/logo.png'))
-            ->favicon(asset('images/favicon.ico'))
+            //->favicon(asset('images/favicon.ico'))
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
